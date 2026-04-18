65c3db2b01458ab2abeda1643db3dbbe0d1cc254
M <mjl@LeoBook.lan>
2026-04-17 22:11:27 -0500

    Add PWA, and update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
